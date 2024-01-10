22779b2d4a500fe56c4d291999c572c84988e933
zmyke05 <papamicheledks@gmail.com>
2024-01-10 14:47:04 +0100

    14:46

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {MatSidenavModule} from '@angular/material/sidenav';
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { ComponentsModule } from './components/components.module';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
@@ -24,6 +25,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     HttpClientModule,
     MatProgressSpinnerModule,
     ComponentsModule,
+    MatSidenavModule,
     SweetAlert2Module.forRoot(),
   ],
   providers: [
