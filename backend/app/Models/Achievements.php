946bb2e1bec7aa916460ffa9128c903ea042d855
chriceto <christianconfettura06@gmail.com>
2024-01-16 12:20:32 +0100

    g

--- backend/app/Models/Achievements.php
+++ backend/app/Models/Achievements.php
@@ -1,11 +1,30 @@
 <?php
+//NON ELIMINARE 
+//<PLACEHOLDER> = dati da cambiare -chris
 
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Routing\PendingSingletonResourceRegistration;
 
 class Achievements extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'game_id',
+        'name',
+        'is_achieved',
+    ];
+
+    public function UsersAchievements(){
+
+        return $this->hasMany(UsersAchievements::class, 'id', 'achievement');
+    }
+
+    public function Games(){
+
+        return $this->hasOne(Games::class, 'PLACEHOLDERORIGIN', 'PLACEHOLDERDESTINATION');   
+    }
 }
