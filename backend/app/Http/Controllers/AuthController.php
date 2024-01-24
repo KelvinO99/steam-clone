937efc0dd80a5ab9043b58ba03dfd31c7e4ea078
Il Burattinaio <91622236+ShadowOfWrath@users.noreply.github.com>
2024-01-24 16:00:37 +0100

    LoginConFrontEnd

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -43,7 +43,7 @@ class AuthController extends Controller
      */
     public function register(Request $request) {
         $validator = Validator::make($request->all(), [
-            'name' => 'required|string|between:5,30',
+            // 'name' => 'required|string|between:5,30',
             'username' => 'required|string|between:5,30|unique:users',
             'email' => 'required|string|email|max:100|unique:users',
             'password' => 'required|string|confirmed|min:8',
