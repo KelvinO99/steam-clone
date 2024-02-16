23775478ae721564a69d2be73ae151bb64d17bca
zmyke05 <papamicheledks@gmail.com>
2024-02-16 11:38:20 +0100

    sas4

--- frontend/src/app/components/components.module.ts
+++ frontend/src/app/components/components.module.ts
@@ -18,8 +18,7 @@ import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { StoreComponent } from './store/store.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { ProductBannersComponent } from '../product-banners/product-banners.component';
-
+import { ProductBannersComponent } from './product-banners/product-banners.component';
 import { MainCarouselComponent } from './main-carousel/main-carousel.component';
 import { UpdatesOfferCardsComponent } from './updates-offer-cards/updates-offer-cards.component';
 import {MatButtonModule} from '@angular/material/button';
