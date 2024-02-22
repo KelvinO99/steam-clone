9d2a41c172f642cd5ccdc0e8126d7c2431ef5149
zmyke05 <papamicheledks@gmail.com>
2024-02-22 14:17:46 +0100

    sas 6

--- frontend/src/app/shared/services/game.service.ts
+++ frontend/src/app/shared/services/game.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
+
+//richiamo URL
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  APIUrl=`${environment.api}/`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getUpdates(params?: {}){
+    return this.http.get<any>(`${URL})
+
+  }
 }
