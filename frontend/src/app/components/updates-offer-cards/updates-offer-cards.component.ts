dc3e6b6b85c558ea77d99e718c5daf91e6c27d8d
zmyke05 <papamicheledks@gmail.com>
2024-03-22 14:43:13 +0100

    FINITO?!

--- frontend/src/app/components/updates-offer-cards/updates-offer-cards.component.ts
+++ frontend/src/app/components/updates-offer-cards/updates-offer-cards.component.ts
@@ -9,76 +9,46 @@ import { Component } from '@angular/core';
 
 
 export class UpdatesOfferCardsComponent {
-  discount_game: any
-  incoming_game: any
-  best_seller_game: any
-  most_reviewed_game: any
-  skip: number = 0;
-  take: number = 10;
-  incoming: boolean = false
-  best_seller: boolean = false
-  discount: boolean = false
-  most_reviewed: boolean = false
 
-  constructor(public gameService: GameService){}
+  games: any;
+  itemsPerPage = 6;
+  pages: number[] = [];
+  currentIndex = 0;
+
+  constructor(public gameService:GameService) {
+
+  }
 
   ngOnInit(){
-    this.getIncoming()
-    this.getBestSeller()
-    this.getDiscount()
-    this.getMostReviewed()
+this.getUpdatesAndOffers();
   }
 
-  getIncoming() {
-    this.gameService.getGames({skip: this.skip, take: this.take, incoming: this.incoming = true}).subscribe({
-      next: (res: any) => {
-        {
-          this.incoming_game = res
-          console.log("getIncoming")
-          console.log(this.incoming_game);
-          
-        }
-      }
-    })
+getUpdatesAndOffers(){
+  this.gameService.getGames({discount: true}).subscribe({
+    next: (res: any) => {
+
+      this.games = res.games;
+      const pageCount = Math.ceil(this.games.length / this.itemsPerPage);
+      this.pages = Array.from({ length: pageCount }, (_, i) => i + 1);
+    }
+  })
+  }
+  getCardsForPage(index: number): any[] {
+    const start = index * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    return this.games.slice(start, end);
   }
 
-  getBestSeller() {
-    this.gameService.getGames({skip: this.skip, take: this.take, best_seller: this.best_seller = true}).subscribe({
-      next: (res: any) => {
-        {
-          this.best_seller_game = res
-          console.log("getBestSeller")
-          console.log(this.best_seller_game);
-          
-        }
-      }
-    })
+  prevPage(): void {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
   }
 
-  getDiscount() {
-    this.gameService.getGames({skip: this.skip, take: this.take, discount: this.discount = true}).subscribe({
-      next: (res: any) => {
-        {
-          this.discount_game = res
-          console.log("getDiscount")
-          console.log(this.discount_game);
-          
-        }
-      }
-    })
-  }
-
-  getMostReviewed() {
-    this.gameService.getGames({skip: this.skip, take: this.take, most_reviewed: this.most_reviewed = true}).subscribe({
-      next: (res: any) => {
-        {
-          this.most_reviewed_game = res
-          console.log("getMostReviewed")
-          console.log(this.most_reviewed_game);
-          
-        }
-      }
-    })
+  nextPage(): void {
+    if (this.currentIndex < this.pages.length - 1) {
+      this.currentIndex++;
+    }
   }
 
 }
