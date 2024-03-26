959a1e9115ae7c5558602ef30905e092612a0609
zmyke05 <papamicheledks@gmail.com>
2024-03-26 17:50:39 +0100

    a

--- frontend/src/app/components/game-page/body-data-game/body-data-game.component.ts
+++ frontend/src/app/components/game-page/body-data-game/body-data-game.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GameService } from 'src/app/shared/services/game.service';
 
 @Component({
   selector: 'app-body-data-game',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./body-data-game.component.scss']
 })
 export class BodyDataGameComponent {
+  routeId!: number;
+  game!: any;
+  offset = 1;
 
+  constructor(public route: ActivatedRoute, public gameService: GameService){}
+
+
+  ngOnInit() {
+    this.routeId = this.route.snapshot.params['id'];
+    this.showGame();
+  }
+
+  showGame() {
+    this.gameService.showGame(this.routeId).subscribe({
+      next: (res: any) => {
+        {
+          this.game = res
+          console.log(this.game);
+        }
+      }
+    })
+  }
 }
