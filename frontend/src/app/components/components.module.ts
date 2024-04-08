c8e145b4323652fc89e5e85e18f205fe87976061
zmyke05 <papamicheledks@gmail.com>
2024-04-08 11:42:38 +0200

    Finito? Si spera di Sì! 08/03/2024 - 11:42

--- frontend/src/app/components/components.module.ts
+++ frontend/src/app/components/components.module.ts
@@ -32,6 +32,8 @@ import { BigButtonsComponent } from './store/big-buttons/big-buttons.component';
 import { GameComponent } from './game-page/game-page.component';
 import { BodyDataGameComponent } from './game-page/body-data-game/body-data-game.component';
 import { BodyNavbarComponent } from './body-navbar/body-navbar.component';
+import { SteamDeckComponent } from './steam-deck/steam-deck.component';
+import { ReviewsComponent } from './game-page/reviews/reviews.component';
 @NgModule({
   declarations: [
     BaseComponent,
@@ -50,7 +52,9 @@ import { BodyNavbarComponent } from './body-navbar/body-navbar.component';
     BigButtonsComponent,
     GameComponent,
     BodyDataGameComponent,
-    BodyNavbarComponent
+    BodyNavbarComponent,
+    SteamDeckComponent,
+    ReviewsComponent,
 
   ],
   imports: [
