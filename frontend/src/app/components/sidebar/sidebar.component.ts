a872f74ca14799eb5a64db97e616b0f06e6732e2
zmyke05 <papamicheledks@gmail.com>
2024-04-09 22:14:25 +0200

    fix-css-body-navbar

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -24,7 +24,7 @@ export class SidebarComponent {
       .getGenres({
         skip: this.skip,
         take: this.take,
-        category: true, // Imposta il flag a true
+        
       })
       .subscribe((res: any) => {
         this.genre = res.tags;
