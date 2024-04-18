a21ac9c7e0a341fcb1631a6bd50ff364d3f1a2c5
chriceto <christianconfettura06@gmail.com>
2024-04-18 09:17:30 +0200

    //

--- backend/app/Http/Controllers/GamesController.php
+++ backend/app/Http/Controllers/GamesController.php
@@ -45,10 +45,9 @@ class GamesController extends Controller
 
 
 
-            if (!empty($tag)) { // Change from $tags to $tag
-                // Loop through each tag and apply the filter
-                foreach ($tag as $t) { // Change from $tags to $tag
-                    $game->whereHas('GamesTags.Tags', function ($q) use ($t) { // Change from $tags to $tag
+            if (!empty($tag)) {
+                foreach ($tag as $t) {
+                    $game->whereHas('GamesTags.Tags', function ($q) use ($t) {
                         $q->where('name', $t);
                     });
                 }
@@ -87,7 +86,7 @@ class GamesController extends Controller
                 $game->where('date', '>', $today);
             }
             if ($new_release) {
-                $game->whereDate('date', '>=', now()->subYears(2));
+                $game->whereDate('date', '>=', now()->subMonths(3));
                 $game->orderBy('date', 'desc');
             }
 
