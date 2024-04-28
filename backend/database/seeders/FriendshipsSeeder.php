6edcc03a50007432ef57133c0eb9b78e5310838e
chriceto <christianconfettura06@gmail.com>
2024-04-28 20:18:13 +0200

    commitdacasa_bitch

--- backend/database/seeders/FriendshipsSeeder.php
+++ backend/database/seeders/FriendshipsSeeder.php
@@ -17,12 +17,13 @@ class FriendshipsSeeder extends Seeder
         // 2.b caso: 2 utenti che hanno più record (1,2,1,0) (1,2,0,1)
         // 3 caso: 2 utenti che hanno 2 record con is_sender | is_receiver scambiati
 
-        for($i = 1; $i <= 10; $i++)
-        {
-            $user_sender =  mt_rand(1,4);
-            $user_receiver = mt_rand(1,4);
-            $flag = 0; //creazione di flag
+        $no_friendships = 1;
 
+        for($i = 1; $i <= 100; $i++)
+        {
+            $user_sender =  mt_rand(1, 32); //32
+            $user_receiver = mt_rand(1, 32); //32
+            $flag = 0; //creazione e reset di flag a 0
 
             if($i != 1) //il primo ciclo non avrà mai conflitti ed è inutile controllare, in + permetterà di creare il primo record dove si attuerà il compare
             {
@@ -46,7 +47,7 @@ class FriendshipsSeeder extends Seeder
                             if($user_sender == $check_sender && $user_receiver == $check_receiver)
                             {
                                 $flag = 1;
-                                dump("beccato un record già esistente nel check (2.a) n: ".$j."!!!\nnon dovrebbero esserci altri cicli con j= ".$j." del ciclo n: ".$i);
+                                dump("beccato un record già esistente nel check (2.a) n: ".$j."!!!\nnon dovrebbero esserci altri cicli dopo j= ".$j." del ciclo n: ".$i);
                             }
                             else
                             {
@@ -57,27 +58,23 @@ class FriendshipsSeeder extends Seeder
 
                     if($flag == 0)
                     {
-                        for($j = 1; $j <= $i; $j++)
-                        {
-                            $friendship = DB::table('friendships')
-                                ->where('id', $j)
-                                ->select('user_sender');
-                            for($k = 1; $k <=)
-                                if(true) // 3 caso //to:do
-                                {
-                                    dump("sto creando friendship...");
-                                    $rng_pending = rand(1, 8);
-                                    $rng_blocked = rand(1, 16);
-                                    DB::table('friendships')->insert([
-                                        'user_sender' => $user_sender,
-                                        'user_receiver' => $user_receiver,
-                                        'is_pending' => $rng_pending == 1 ? 1 : 0,
-                                        'is_blocked' => $rng_blocked == 1 ? 1 : 0,
-                                    ]);
-                                    dump("ho creato friendship. raw values: "
-                                    ."id: ".$i." user_sender: ".$user_sender." user_receiver: ".$user_receiver." rng_pending: ".$rng_pending." rng_blocked: ".$rng_blocked);
-                                }
+                        dump("sto creando friendship...");
+                        $rng_pending = rand(1, 8);
+                        $rng_blocked = rand(1, 16);
+                        DB::table('friendships')->insert([
+                            'user_sender' => $user_sender,
+                            'user_receiver' => $user_receiver,
+                            'is_pending' => $rng_pending == 1 ? 1 : 0,
+                            'is_blocked' => $rng_blocked == 1 ? 1 : 0,
+                        ]);
+                        dump("ho creato friendship. raw values: "
+                        ."id: ".$i." user_sender: ".$user_sender." user_receiver: ".$user_receiver." rng_pending: ".$rng_pending." rng_blocked: ".$rng_blocked);
+                        $no_friendships++;
                     }
+                }
+                else
+                {
+                    dump("beccato un record con is_sender e is_receiver uguali");
                 }
             }
             else
@@ -93,6 +90,44 @@ class FriendshipsSeeder extends Seeder
                 ]);
                 dump("ho creato la prima friendship. raw values: "
                 ."id: ".$i." user_sender: ".$user_sender." user_receiver: ".$user_receiver." rng_pending: ".$rng_pending." rng_blocked: ".$rng_blocked);
+            }
+        }
+        dump($no_friendships);
+        for($i = 1; $i <= $no_friendships; $i++)
+        {
+            for($j = 1; $j < $no_friendships; $j++)
+            {
+
+                $a = DB::table('friendships')
+                        ->where('user_sender', $i)
+                        ->where('user_receiver', $j)
+                        ->first();
+
+                        if ($a) {
+                            $a_sender = $a->user_sender;
+                            $a_receiver = $a->user_receiver;
+                        }
+                        else continue;
+
+                $b = DB::table('friendships')
+                        ->where('user_sender', $j)
+                        ->where('user_receiver', $i)
+                        ->first();
+
+                        if ($b) {
+                            $b_sender = $b->user_sender;
+                            $b_receiver = $b->user_receiver;
+                        }
+                        else continue;
+
+                if($a_sender == $b_receiver && $a_receiver == $b_sender)
+                {
+                    DB::table('friendships')
+                        ->where('user_sender', $j)
+                        ->where('user_receiver', $i)
+                        ->delete();
+
+                }
             }
         }
     }
