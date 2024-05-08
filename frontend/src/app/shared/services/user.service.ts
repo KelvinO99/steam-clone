06823872ff618f499ac301ee9fc5b12ae55429a7
zmyke05 <papamicheledks@gmail.com>
2024-05-08 09:48:59 +0200

    modifiche salvate da michele per cambiare branch

--- frontend/src/app/shared/services/user.service.ts
+++ frontend/src/app/shared/services/user.service.ts
@@ -1,13 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+
+//Variabile per il richiamo dell'API
+const URL: string = `${environment.api}`;
 
 @Injectable({
   providedIn: 'root',
 })
 export class User {
+  APIUrl = `${environment.api}/`;
   _id?: String;
   name?: String;
   email?: String;
   password?: String;
+
+  //Costruttore per il client HTTP
+  constructor(public http: HttpClient) { }
+
+  getGames(params : {}) {
+    return this.http.get<any>(`${this.APIUrl}games/index`,{params});
+  }
+
+  showGame(id: number){
+    return this.http.get<any>(`${this.APIUrl}games/show/${id}`);
+  }
+
+
 }
+
+
